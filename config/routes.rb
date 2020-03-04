3fb9f648a0fd94a96f95d8dd812a56b8525c7915
jules le morvan jules.beauterra^gmail.com <jules.beauterra@gmail.com>
2020-03-04 10:52:02 +0100

    schedule

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  get 'pages/schedule'
   resources :favorites, only: [:create, :destroy]
   # get 'favorites/create'
   # get 'favorites/destroy'
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   # get 'films/show'
   # get 'films/index'
 
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
