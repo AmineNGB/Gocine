6eb6bef5cdbf6f0f8d7c6af38c8b2ed004947fc4
Nicolas Iniesta <inicolas69@gmail.com>
2020-03-04 17:15:50 +0100

    page profile finie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,17 @@ User.destroy_all
 user = User.new
 user.email = 'amine@gmail.com'
 user.password = 'password'
+user.nom = 'Neghbel'
+user.prenom = 'Amine'
+user.phone = '06xxxxxxxx'
 user.save!
 
 user2 = User.new
 user2.email = 'nico@gmail.com'
 user2.password = 'password'
+user2.nom = 'Iniesta'
+user2.prenom = 'Nicolas'
+user2.phone = '07xxxxxxxx'
 user2.save!
 
 puts 'Creating Films...'
