b6e6c5a11809fa7f3939246b04edf383967812ad
Amine <amineneghbel@gmail.com>
2020-03-06 14:06:17 +0100

    ajout de films dans mes favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,18 @@
 class FavoritesController < ApplicationController
-  before_action :set_favorite, only: [:show, :destroy]
+  before_action :set_film, only: [:show, :destroy]
+
   def index
+    @favorites = Favorite.all
+  end
+
+  def new
+    @favorite = Favorite.new
   end
 
   def create
-    @favorite = Favorite.new(favorites_params)
-    @user = current_user
-    @favorite.save!
+    @favorite = Favorite.create!(user_id: current_user.id, film_id: params[:film_id])
+
+    redirect_to favorites_path
   end
 
   def destroy
@@ -21,7 +27,7 @@ class FavoritesController < ApplicationController
     params.require(:favorite).permit(:user_id, :film_id)
   end
 
-  def set_favorite
-    @favorite = Favorite.find(params[:id])
+  def set_film
+    @film = Film.find(params[:id])
   end
 end
