c6d4605f50004bbd699517e54bda30c6ee11f47b
Amine <amineneghbel@gmail.com>
2020-06-19 18:25:41 +0200

    javascript bouton +

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,7 @@ initSortable();
 const icon = document.querySelector('#movie-icon');
 
 icon.addEventListener("click", () => {
-    if (icon.classList == "fas fa-plus-square fa-3x")
-        icon.classList.replace("fas fa-plus-square fa-3x", "fas fa-plus-minus fa-3x") 
+    icon.classList.replace("fas fa-plus-square fa-3x", "fas fa-plus-minus fa-3x") 
 })
-
+// il semblerait que le script ne passe pas a verifier
 
