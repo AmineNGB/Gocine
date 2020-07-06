7e00e5f3112efd59e5d180cbc61e528da41b5f39
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-06 16:41:17 +0200

    Ajout traduction FR pour Devise (a configurer)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,12 +9,15 @@ Bundler.require(*Rails.groups)
 module Gocine
   class Application < Rails::Application
     config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}")]
+    config.i18n.default_locale = :fr
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
