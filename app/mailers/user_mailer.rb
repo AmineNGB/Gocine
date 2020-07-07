4573ecac5699e0ddf9d05c6911ca482e38f57b0f
amineneghbel <amineneghbel@gmail.com>
2020-07-07 18:50:32 +0200

    mailer pour inviter ses amis au cinema fait avec active mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+    default from: 'gocineclub@gmail.com'
+ 
+  def ask_for_event(event, url)
+    @guests_all = event.guests
+    @url = url
+    @favorites = favorites_url
+    @guests = @guests_all.reverse.drop(1).reverse
+    @guests.each do |guest| 
+        @guest = guest
+        mail(to: guest.user.email, subject: 'Veux-tu participer à la sortie Cinéma ?')
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require "open-uri"
 require "nokogiri"
 require "awesome_print"
-
 # cinemas = Cinema.all
 # cinemas.each do |cinema|
 # cinema.create_movies
@@ -12,41 +11,40 @@ Seance.destroy_all
 Favorite.destroy_all
 Cinema.destroy_all
 Event.destroy_all
-# "User in progress ..."
-# user = User.new
-# user.email = "amine@gmail.com"
-# user.password = "password"
-# user.nom = "Neghbel"
-# user.prenom = "Amine"
-# user.phone = "06xxxxxxxx"
-# user.save!
-
-# user2 = User.new
-# user2.email = "nico@gmail.com"
-# user2.password = "password"
-# user2.nom = "Iniesta"
-# user2.prenom = "Nicolas"
-# user2.phone = "07xxxxxxxx"
-# user2.save!
-
-# user3 = User.new
-# user3.email = "boule@gmail.com"
-# user3.password = "password"
-# user3.nom = "Bou"
-# user3.prenom = "Boule"
-# user3.phone = "07xxxxxxxx"
-# user3.save!
-
-# user4 = User.new
-# user4.email = "jules@gmail.com"
-# user4.password = "password"
-# user4.nom = "Ju"
-# user4.prenom = "Jules"
-# user4.phone = "07xxxxxxxx"
-# user4.save!
-
+"User in progress ..."
+user = User.new
+user.email = "amine@gmail.com"
+user.password = "password"
+user.nom = "Neghbel"
+user.prenom = "Amine"
+user.phone = "06xxxxxxxx"
+user.skip_confirmation!
+user.save!
+user2 = User.new
+user2.email = "nico@gmail.com"
+user2.password = "password"
+user2.nom = "Iniesta"
+user2.prenom = "Nicolas"
+user2.phone = "07xxxxxxxx"
+user2.skip_confirmation!
+user2.save!
+user3 = User.new
+user3.email = "boule@gmail.com"
+user3.password = "password"
+user3.nom = "Bou"
+user3.prenom = "Boule"
+user3.phone = "07xxxxxxxx"
+user3.skip_confirmation!
+user3.save!
+user4 = User.new
+user4.email = "jules@gmail.com"
+user4.password = "password"
+user4.nom = "Ju"
+user4.prenom = "Jules"
+user4.phone = "07xxxxxxxx"
+user4.skip_confirmation!
+user4.save!
 "Creation des films à l\'affiche"
-
 cinema_attributes = [
   # {
   #   name: 'Les Amphis',
@@ -74,7 +72,6 @@ cinema_attributes = [
     allocine_id: "P8507",
   },
 ]
-
 cinema_attributes.each do |attr|
   ap "---------------------------"
   cinema = Cinema.create!(attr)
@@ -82,7 +79,6 @@ cinema_attributes.each do |attr|
   cinema.create_movies
   ap "---------------------------"
 end
-
 # favorites_attributes = [
 #   {
 #     user_id: user.id,
@@ -149,9 +145,7 @@ end
 #     film_id: 4,
 #   },
 # ]
-
 # Favorite.create!(favorites_attributes)
-
 # events_attributes = [
 #   {
 #     schedule: :matin,
@@ -162,30 +156,21 @@ end
 # ]
 # "creation evenements"
 # Event.create!(events_attributes)
-
 # Guest.create!(user_id: 1, event_id: 1, status: 1)
 # Guest.create!(user_id: 2, event_id: 1, status: 1)
 # Guest.create!(user_id: 3, event_id: 1, status: 0)
 # Guest.create!(user_id: 4, event_id: 1, status: 2)
-
-# user.friend_request(user2)
-# user2.accept_request(user)
-
-# user.friend_request(user3)
-# user3.accept_request(user)
-
-# user.friend_request(user4)
-# user4.accept_request(user)
-
-# user2.friend_request(user3)
-# user3.accept_request(user2)
-
-# user2.friend_request(user4)
-# user4.accept_request(user2)
-
-# user3.friend_request(user4)
-# user4.accept_request(user3)
-
+user.friend_request(user2)
+user2.accept_request(user)
+user.friend_request(user3)
+user3.accept_request(user)
+user.friend_request(user4)
+user4.accept_request(user)
+user2.friend_request(user3)
+user3.accept_request(user2)
+user2.friend_request(user4)
+user4.accept_request(user2)
+user3.friend_request(user4)
+user4.accept_request(user3)
 # ap Favorite.all
-
 # ap Event.all
