2a95bf3e16975f13b8ede86ac97254be15e96854
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-13 13:16:33 +0200

    Ajout badge 5*

--- app/models/film.rb
+++ app/models/film.rb
@@ -8,4 +8,12 @@ class Film < ApplicationRecord
   def new?
     self.date_release > Date.today - 7 if self.date_release
   end
+
+  def top?
+    if self.rate_viewer && self.rate_press
+      ((self.rate_press + self.rate_viewer) / 2) > 4
+    elsif self.rate_press
+      self.rate_press > 4
+    end
+  end
 end
