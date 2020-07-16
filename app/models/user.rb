f0181eca5aa329c481530b84f322441ae408985e
Nicolas Iniesta <inicolas69@gmail.com>
2020-07-16 16:45:10 +0200

    FB connect ajouté

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  devise :omniauthable, omniauth_providers: %i[facebook]
+
   def to_s
     "#{prenom} #{nom}"
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      ap auth.info
+      user.prenom = auth.info.name.split(" ")[0]   # assuming the user model has a name
+      user.nom = auth.info.name.split(" ")[1]
+      # user.image = auth.info.image # assuming the user model has an image
+      user.skip_confirmation!
+    end
   end
 
   def pending_events
